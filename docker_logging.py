0b6a38cba6c81fbe7d8b503a48707127097484e8
w1nn3rpy <l33sa1337@gmail.com>
2024-09-26 20:57:10 +0400

    fix

--- docker_logging.py
+++ docker_logging.py
@@ -11,6 +11,7 @@ def stream_container_logs():
     try:
         container = client.containers.get(container_name)
         for line in container.logs(stream=True):
+            print(line)
             yield line.decode('utf-8')
 
     except Exception as e:
