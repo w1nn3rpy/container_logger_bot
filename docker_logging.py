75ac31de45c7d7a3d79c13e603523fa8603789a4
w1nn3rpy <l33sa1337@gmail.com>
2024-09-26 21:04:09 +0400

    fix

--- docker_logging.py
+++ docker_logging.py
@@ -10,9 +10,19 @@ container_name = "dudevpn_bot"
 def stream_container_logs():
     try:
         container = client.containers.get(container_name)
+        buffer = ""  # Буфер для накопления символов
         for line in container.logs(stream=True):
-            print(line)
-            yield line.decode('utf-8')
+            for char in line.decode("utf-8"):
+                if char == '\n':  # Если встретили символ новой строки
+                    yield buffer.strip()  # Отправляем собранную строку и очищаем буфер
+                    buffer = ""  # Очищаем буфер для следующей строки
+                else:
+                    buffer += char  # Добавляем символ в буфер
+
+        # Если есть остатки в буфере, отправляем их
+        if buffer:
+            yield buffer.strip()
 
     except Exception as e:
-        return f'Ошибка при получении логов: {e}'
+        yield f'Ошибка при получении логов: {e}'
+
