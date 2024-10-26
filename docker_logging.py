520f1a2ce1c5b663d4e6f8ff787fe1e5a2ca3af4
w1nn3rpy <l33sa1337@gmail.com>
2024-10-26 10:08:58 +0400

    update log system

--- docker_logging.py
+++ docker_logging.py
@@ -8,22 +8,6 @@ client = docker.from_env()
 # Указываем имя контейнера
 container_name = "dudevpn_bot"
 
-LOG_EXPIRY_TIME = timedelta(minutes=5)
-
-log_queue = deque()
-
-def add_log(log):
-    # Добавляем лог с текущим временем
-    log_queue.append((log, datetime.now()))
-    # Удаляем старые логи
-    remove_expired_logs()
-
-
-def remove_expired_logs():
-    while log_queue and (datetime.now() - log_queue[0][1] > LOG_EXPIRY_TIME):
-        log_queue.popleft()  # Удаляем самый старый лог, если он просрочен
-
-
 
 def stream_container_logs():
     try:
@@ -34,7 +18,6 @@ def stream_container_logs():
                 if char == '\n':  # Если встретили символ новой строки
                     log_message =  buffer.strip()  # Отправляем собранную строку и очищаем буфер
                     yield log_message
-                    add_log(log_message)
                     buffer = ""  # Очищаем буфер для следующей строки
                 else:
                     buffer += char  # Добавляем символ в буфер
@@ -43,7 +26,6 @@ def stream_container_logs():
         if buffer:
             log_message = buffer.strip()
             yield log_message
-            add_log(log_message)
 
     except Exception as e:
         yield f'Ошибка при получении логов: {e}'
