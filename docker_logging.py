bcaabd326a373f1c036bef4d8f0a9595a239e169
w1nn3rpy <l33sa1337@gmail.com>
2025-01-13 19:57:28 +0400

    edit

--- docker_logging.py
+++ docker_logging.py
@@ -12,9 +12,15 @@ def stream_container_logs():
         container = client.containers.get(container_name)
         buffer = ""  # Буфер для накопления символов
         for line in container.logs(stream=True):
-            for char in line.decode("utf-8"):
+            try:
+                decoded_line = line.decode("utf-8", errors="replace")  # Заменяем некорректные символы
+            except Exception as decode_error:
+                yield f"Ошибка декодирования строки: {decode_error}"
+                continue
+
+            for char in decoded_line:
                 if char == '\n':  # Если встретили символ новой строки
-                    log_message =  buffer.strip()  # Отправляем собранную строку и очищаем буфер
+                    log_message = buffer.strip()  # Отправляем собранную строку и очищаем буфер
                     yield log_message
                     buffer = ""  # Очищаем буфер для следующей строки
                 else:
@@ -26,5 +32,4 @@ def stream_container_logs():
             yield log_message
 
     except Exception as e:
-        yield f'Ошибка при получении логов: {e}'
-
+        yield f"Ошибка при получении логов: {e}"
